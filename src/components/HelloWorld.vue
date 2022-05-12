48a4d8fac150288a5013cf791393886f89c51f9a
Devin <xiangqun.xiao@poper.co>
2022-05-12 09:48:41 +0800

    eslint error

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  defineProps<{ msg: string }>();
+
+  ElMessage({
+    message: 'Congrats, this is a success message.',
+    type: 'success',
+  });
+  const components = [
+    {
+      name: ElInput,
+      attrs: {
+        placeholder: ' ID',
+        clearable: true,
+      },
+    },
+    {
+      name: ElTag,
+      attrs: {
+        type: 'success',
+        text: 1023,
+      },
+    },
+  ];
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <el-button type="primary">66666</el-button>
+  <div v-for="(props, i) in components" :key="i">
+    <component v-bind="props.attrs" :is="props.name">
+      {{ props.attrs.text }}
+    </component>
+  </div>
+</template>
